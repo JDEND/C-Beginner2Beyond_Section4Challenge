0c5ea6a94182c3eef3689a3fc073d09a03918c0c
Jared Endicott <Jedijde@gmail.com>
2022-08-03 22:30:42 -0400

    Add project files.

---
+++ C++Beginner2Beyond_Section4Challenge/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+	int temp;
+
+	cout << "Enter a number: ";
+	cin >> temp;
+	cout << "\nAmazing!! That's my favorite number too!\nNo really!!, " << temp << " is my favorite number!" << endl;
+
+
+
+	return 0;
+}
